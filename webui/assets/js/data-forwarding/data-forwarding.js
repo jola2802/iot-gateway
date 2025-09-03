d4a5c4f317bdded421eb19bb871beb15b1bd2a97
jola2802 <jonas.lanzl@web.de>
2025-09-03 09:37:20 +0200

    f

--- webui/assets/js/data-forwarding/data-forwarding.js
+++ webui/assets/js/data-forwarding/data-forwarding.js
@@ -5,7 +5,7 @@ async function loadNodeRedURLFlow() {
         const baseUrl = currentUrl.split('/').slice(0, -1).join('/');
         const nodeRedURLFlow = document.getElementById('node-red-data-forwarding-flow');
         if (nodeRedURLFlow) {
-            nodeRedURLFlow.src = `${baseUrl}/nodered`;
+            nodeRedURLFlow.src = "http://127.0.0.1:1880";
         }
     } catch (error) {
         console.error('Fehler beim Laden der Node-RED URL:', error);
